cc0da85940eed7437a0afcfb300604265944c802
alphaNewrex <f20201712@goa.bits-pilani.ac.in>
2022-10-25 19:42:25 +0530

    initial commit

---
+++ CMakeFiles/pdi.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pdi.dir/Color.cpp.o"
+  "CMakeFiles/pdi.dir/Color.cpp.o.d"
+  "CMakeFiles/pdi.dir/Filter.cpp.o"
+  "CMakeFiles/pdi.dir/Filter.cpp.o.d"
+  "CMakeFiles/pdi.dir/Main.cpp.o"
+  "CMakeFiles/pdi.dir/Main.cpp.o.d"
+  "CMakeFiles/pdi.dir/Noise.cpp.o"
+  "CMakeFiles/pdi.dir/Noise.cpp.o.d"
+  "pdi"
+  "pdi.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pdi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
